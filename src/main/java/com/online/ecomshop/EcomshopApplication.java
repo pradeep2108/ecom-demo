9f4a17e91071a744197f7cfad0c07961ccacdfeb
pradeep2108 <Pradeepr0821@gmail.com>
2024-01-07 17:57:13 +0530

    Initialized Spring project and established foundational product model

---
+++ src/main/java/com/online/ecomshop/EcomshopApplication.java
@@ -0,0 +1,13 @@
+package com.online.ecomshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomshopApplication.class, args);
+	}
+
+}
